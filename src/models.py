fd05e34e65a3adfe2b2029a5ec73aacd81cd50ff
Ilija Frankovic <ifwopf@gmail.com>
2019-10-18 21:58:04 +0200

    url4

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, func
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.orm import sessionmaker, relationship, backref, contains_eager, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.ext.hybrid import hybrid_property
 from itsdangerous import URLSafeTimedSerializer
 from datetime import datetime
@@ -11,8 +12,11 @@ import random
 #login_serializer = URLSafeTimedSerializer(app.secret_key)
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
-engine = create_engine('postgresql://postgres:dfghp@localhost:5432/urlaubskalender', echo=True)
+#postgresql-parallel-70450
+#localhost:5432/urlaubskalender
+engine = create_engine('postgres://bfnsdjdsjpjxah:2804230acb68e09e908d80e0b47b03697737a5d45b8f58b819c4ea6f8d2dcff0@ec2-54-225-115-177.compute-1.amazonaws.com:5432/d1ue7m4qfl7vg9', echo=True)
 Sess = scoped_session(sessionmaker(bind=engine))
+
 sess = Sess()
 
 
@@ -23,6 +27,27 @@ class User(Base):
     password = Column(String(255))
     name = Column(String(255))
     timestamp = Column(DateTime())
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = generate_password_hash(password, method='sha256')
+
+    @classmethod
+    def authenticate(cls, **kwargs):
+        email = kwargs.get('email')
+        password = kwargs.get('password')
+
+        if not email or not password:
+            return None
+
+        user = sess.query(cls).filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            return None
+
+        return user
+
+    def to_dict(self):
+        return dict(id=self.id, email=self.email)
 
 
 class Categeory(Base):
@@ -46,4 +71,4 @@ class Day(Base):
     user = Column(Integer)
     name = Column(String(255))
 
-Base.metadata.create_all(engine)
+#Base.metadata.create_all(engine)
--- urlaubskal.py
+++ urlaubskal.py
@@ -1,16 +1,81 @@
 from flask import Flask, render_template, request, json, jsonify
-from src.models import sess, User, Categeory, Day, datetime
+from functools import wraps
+from src.models import sess, User, Categeory, Day, datetime, Base, metadata, engine
 from sqlalchemy import and_, or_
 from flask_cors import CORS
+import jwt
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/urlaub/api/*": {"origins": "*"}})
 
+def token_required(f):
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registeration and / or authentication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+        print(auth_headers)
+        if len(auth_headers) != 2:
+            print('hereee')
+            return jsonify(invalid_msg), 401
+
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, 'secretKeyShouldBeinConfigups')
+            user = sess.query(User).filter_by(email=data['sub']).first()
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            print('here')
+            return jsonify(expired_msg), 401 # 401 is Unauthorized HTTP status code
+        except (jwt.InvalidTokenError, Exception) as e:
+            print('here2')
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
+
+@app.route('/urlaub/api/v1.0/register/', methods=('POST',))
+def register():
+    data = request.get_json()
+    user = User(**data)
+    sess.add(user)
+    sess.commit()
+    return jsonify(user.to_dict()), 201
+
+@app.route('/urlaub/api/v1.0/login/', methods=('POST',))
+def login():
+    data = request.get_json()
+    user = User.authenticate(**data)
+
+    if not user:
+        return jsonify({ 'message': 'Invalid credentials', 'authenticated': False }), 401
+    token = jwt.encode({
+        'sub': user.email,
+        'iat':datetime.utcnow(),
+        'exp': datetime.utcnow() + timedelta(minutes=1440)},
+        'secretKeyShouldBeinConfigups')
+    return jsonify({ 'token': token.decode('UTF-8') })
 
 @app.route('/urlaub/api/v1.0/days/<year>', methods=['GET'])
+#@token_required verify,
 def get_days(year):
+    print('year' + year)
     rows = sess.query(Day, Categeory).filter(and_(Day.user == 0, Day.year == year)) \
         .filter(Day.category == Categeory.id).all()
+    if len(rows) == 0:
+        createYear(year)
+        rows = sess.query(Day, Categeory).filter(and_(Day.user == 0, Day.year == year)) \
+            .filter(Day.category == Categeory.id).all()
     list = [[], [], [], [], [], [], [], [], [], [], [], []]
     for entry in rows:
         list[(entry[0].month - 1)].append(
@@ -37,6 +102,7 @@ def get_days(year):
 
 
 @app.route('/urlaub/api/v1.0/change_cat', methods=['POST'])
+#@token_required verify
 def change_cat():
     days = request.json['days']
     cat_id = request.json["cat_id"]
@@ -77,6 +143,26 @@ def editCat():
     sess.commit()
     return "ok"
 
+@app.route('/urlaub/api/v1.0/editCatName', methods=['POST'])
+def editCatName():
+    cat_name = request.json["catName"]
+    cat_id = request.json["catId"]
+    cat = sess.query(Categeory).filter(Categeory.id == cat_id).first()
+    if cat_name != '':
+        cat.name = cat_name
+    sess.commit()
+    return "ok"
+
+@app.route('/urlaub/api/v1.0/editCatColor', methods=['POST'])
+def editCatColor():
+    cat_color = request.json["catColor"]
+    cat_id = request.json["catId"]
+    cat = sess.query(Categeory).filter(Categeory.id == cat_id).first()
+    if cat_color != '':
+        cat.color = cat_color
+    sess.commit()
+    return "ok"
+
 @app.route('/urlaub/api/v1.0/deleteCat', methods=['POST'])
 def deleteCat():
     catID = request.json["catID"]
@@ -90,6 +176,78 @@ def deleteCat():
         return "It's done. RIP"
     else:
         return "Default Cat! Cannot Delete"
+
+@app.route('/urlaub/api/v1.0/createDB', methods=['POST'])
+def createDB():
+    Base.metadata.create_all(engine)
+
+
+def createYear(year):
+    monate = ["Januar", "Februar", "Maerz", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober",
+              "November", "Dezember"]
+    januar = {}
+    februar = {}
+    maerz = {}
+    april = {}
+    mai = {}
+    juni = {}
+    juli = {}
+    august = {}
+    september = {}
+    oktober = {}
+    november = {}
+    dezember = {}
+    einunddreisig = [januar, maerz, mai, juli, august, oktober, dezember]
+    dreisig = [april, juni, september, november]
+    achtundzwanzig = [februar]
+    jahr = [januar, februar, maerz, april, mai, juni, juli, august, september, oktober, november, dezember]
+    wochentag = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+    for ele in einunddreisig:
+        i = 0
+        while i < 31:
+            i = i + 1
+            ele[i] = []
+
+    for ele in dreisig:
+        i = 0
+        while i < 30:
+            i = i + 1
+            ele[i] = []
+
+    if year == "2020":
+        lenFeb = 29
+    else:
+        lenFeb = 28
+    for ele in achtundzwanzig:
+        i = 0
+        while i < lenFeb:
+            i = i + 1
+            ele[i] = []
+    startTag = {"2019": 1, "2020":2, "2021":4}
+    j = startTag[str(year)]
+    for monat in jahr:
+        for tag in monat:
+            try:
+                monat[tag].append(wochentag[j])
+                if j==6:
+                    j = 0
+                else:
+                    j += 1
+            except:
+                pass
+    print(jahr)
+
+    m = 0
+    d = 0
+    for monat in jahr:
+        for tag in monat:
+            d += 1
+            new_day = Day(day=d, month=m + 1, year=int(year), category=1, weekday=jahr[m][d][0], user=0)
+            sess.add(new_day)
+        m += 1
+        d = 0
+    sess.commit()
+
 
 
 if __name__ == '__main__':
