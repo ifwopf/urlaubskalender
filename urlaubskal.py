0c6d31c8502a36bd431440878b670b8fddce013a
Ilija Frankovic <ifwopf@gmail.com>
2020-06-07 12:43:04 +0200

    data aws

--- urlaubskal.py
+++ urlaubskal.py
@@ -90,7 +90,8 @@ def addCalendarUnreg(years, cats, user):
         for month in year:
             for day in month:
                 if day["note"] is not None or day["cat_id"] != 0:
-                    newUserday = Userday(dayID=day["id"], value=1, catID = catOldIDDict[day["cat_id"]], calID=newCal.id, name=day["note"])
+                    dbDay = sess.query(Day).filter(Day.day==day["day"], Day.month==day["month"], Day.year==day["year"]).first()
+                    newUserday = Userday(dayID=dbDay.id, value=1, catID = catOldIDDict[day["cat_id"]], calID=newCal.id, name=day["note"])
                     sess.add(newUserday)
     sess.commit()
 
